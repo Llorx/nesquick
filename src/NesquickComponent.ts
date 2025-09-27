9a8c51958881a5405ea65386396c931ce508a062
Llorx <dallorx@gmail.com>
2025-09-27 18:28:24 +0200

    fix: Do not render refs as undefined in normal components

--- src/NesquickComponent.ts
+++ src/NesquickComponent.ts
@@ -150,7 +150,7 @@ export class NesquickComponent<P extends ComponentProps = {}> {
     };
     private _renderProps(element:Element, props:ComponentProps) {
         for (const k in props) {
-            if (k !== "children" && k !== "xmlns") {
+            if (k !== "children" && k !== "xmlns" && k !== "ref") {
                 if (typeof props[k] === "function") {
                     if (k.startsWith("on")) {
                         // TODO: Validate events
